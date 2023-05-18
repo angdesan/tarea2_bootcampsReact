7be1b70f0c81bb997ce38d4b2bfd55a5fabb40c7
angdesan <angdesan99@gmail.com>
2023-05-17 21:48:54 -0500

    tarea finalizada

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+import React, { useState, useRef } from 'react';
 import './App.css';
+import constantes from './constantes/constantes';
+const App = () => {
+  
+  let dragItem = useRef();
+  let dragOverItem = useRef();
+  const [list, setList] = useState(constantes.lista);
+ 
+  const dragStart = (e, position) => {
+    dragItem.posicion = position;
+  };
+ 
+  const dragEnter = (e, position) => {
+    dragOverItem.posicion = position;
+  };
+ /**
+  * 
+  * @param {*} e evento que genera el drop
+  * función que maneja el evento drop realizando una copia de la lista
+  * y eliminando el elemento seleccionado y agregandolo en la posicion 
+  * donde se encuentre al final del "arrastre"
+  */
+  const drop = (e) => {
+    const copiaListaItem = [...list];
+    const dragElement = copiaListaItem[dragItem.posicion];
+    copiaListaItem.splice(dragItem.posicion,1);
+    copiaListaItem.splice(dragOverItem.posicion,0, dragElement);
+    dragItem.posicion = null;
+    dragOverItem.posicion = null;
+    setList(copiaListaItem);
 
-function App() {
+  };
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    {
+    list &&
+    list.map((item, index) => (
+      <div style={{backgroundColor:'lightblue', margin:'20px 25%', textAlign:'center', fontSize:'40px'}}
+        onDragStart={(e) => dragStart(e, index)}
+        onDragEnter={(e) => dragEnter(e, index)}
+        onDragEnd={drop}
+        key={index}
+        draggable> 
+          {item}
+      </div>
+      ))}
+    </>
   );
-}
-
+};
 export default App;
